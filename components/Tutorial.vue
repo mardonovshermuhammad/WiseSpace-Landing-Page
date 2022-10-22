0223fdb621aae01a673896eaf369c0dee54e5192
mardonovshermuhammad <mardonovshermuhammad@gmail.com>
2022-10-22 20:18:36 +0500

    project start

---
+++ components/Tutorial.vue
@@ -0,0 +1,20 @@
+<template>
+      <div>
+        <h1 class=" text-red-600">{{ counts }}</h1>
+      <button class="aaa" @click="count"> count</button>
+      </div>
+</template>
+
+<script setup lang="ts">
+import { ref,Ref  } from "vue";
+const counts:Ref<number> = ref(0)
+  function count() {
+    counts.value++;
+  }
+</script>
+<style scoped>
+.aaa{
+  font-family: 'Bold', sans-serif  ;
+  font-weight: 900;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+ <div>
+  <Tutorial/>
+ </div>
+</template>
+
+<script setup lang="ts">
+import Tutorial from '@/components/Tutorial.vue'
+
+</script>
