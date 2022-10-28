d2388574d3a5b72544164ca279311382f907df91
shakhzod-star <77914936+shakhzod-star@users.noreply.github.com>
2022-10-28 10:34:34 +0500

    MiniFooter

---
+++ components/MiniFooter.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+
+</script>
+
+<template>
+<div class=" bg-bgBtn ">
+    <div class="flex  md:flex-row  flex-col justify-between md:px-20 md:py-8  md:m-auto px-7 py-4 max-w-[1280px]">
+        <div class="text-white md:mb-0  mb-5">
+    2022 Wisesoace.uz
+   </div>
+   <div class="flex md:mb-0  mb-5 ">
+    <div class="text-white mr-7">Terms & Conditions  </div>
+    <div class="text-white">Privacy & Policy </div>
+   </div>
+   <div class="flex items-center">
+     <img class="mr-5" src="../assets/icons/facebook.svg" alt="">
+     <img class="mr-5" src="../assets/icons/instagram.svg" alt="">
+     <img class="mr-5" src="../assets/icons/in.svg" alt="">
+     <img src="../assets/icons/instagram.svg" alt="">
+   </div>
+    </div>
+</div>
+
+</template>
