800725b020fa5d9cd1ae5a783f05323d3b23a510
mardonovshermuhammad <mardonovshermuhammad@gmail.com>
2022-10-30 17:05:31 +0500

    validate the end

--- components/getInTouch/baseInput.vue
+++ components/getInTouch/baseInput.vue
@@ -1,42 +1,187 @@
 <script setup lang="ts">
-import { ref, useAttrs, defineEmits } from "vue";
-const attrs = useAttrs();
-const props = defineProps(["modelValue", 'regex', 'successMsg', 'errorMsg','labelName']);
-
-const emit = defineEmits(['update:modelValue']);
-let val = ref<string>('')
-let Message = ref<string>('')
-let validate = ref<boolean>(false)
-function emitInput(event: any) {
-    console.log(event.target.value);
-    emit('update:modelValue', event.target.value)
-    const regex = props.regex
-    val.value = event.target.value
-    if (val.value === "") {
-        Message.value = 'this field reqired'
-
+import { ref,watch, watchEffect} from "vue";
+const FurstName = ref<string>('')
+const EmailValue = ref<string>('')
+const NumberValue = ref<string>('')
+const organition = ref<string>('')
+const messages = ref<string>('')
+const disabled = ref<boolean>(false)
+const Items = ref<any[]>([
+    { 
+        wer: FurstName,
+        regex: /([a-zA-Z]{3,30}\s*)+/,
+        successMsg:"All is good",
+        errorMsg:"text must be Full name",
+        label:"Full name",
+        Message: "",
+        val: '',
+        validate: false
+        
+     },
+    {   
+        wer: EmailValue,
+        regex: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+        successMsg:"All is good",
+        errorMsg:"text must be email",
+        label:"Email",
+        Message: "",
+        val: '',
+        validate: false
+    },
+    {   
+        wer: NumberValue,
+        regex: /^[+]?998[012345789][0-9]{8}$/,
+        successMsg:"All is good",
+        errorMsg:"text must be Uzb Phone",
+        label:"Phone Number",
+        Message: "",
+        val: '',
+        validate: false
+    },
+    { 
+        wer: organition,
+        regex: /([a-zA-Z]{3,80}\s*)+/,
+        successMsg:"All is good",
+        errorMsg:"text must be Full name",
+        label:"Organisation name",
+        Message: "",
+        val: '',
+        validate: false
+        
+     },
+     { 
+        wer: messages,
+        regex: /([a-zA-Z])+/,
+        successMsg:"All is good",
+        errorMsg:"text must be message",
+        label:"message",
+        Message: "",
+        val: '',
+        validate: false
+        
+     },
+])
+    watch(FurstName,()=>{
+     const regex = Items.value[0].regex
+       Items.value[0].val = FurstName.value
+    if (FurstName.value == "") {
+        Items.value[0].Message = 'this field reqired'
     }
-    else if (regex.test(val.value)) {
-        validate.value = true
-        Message.value = props.successMsg
-
+    else if (regex.test(FurstName.value)) {
+        Items.value[0].validate = true
+        Items.value[0].Message = Items.value[0].successMsg
     }
     else {
-        validate.value = false
-        Message.value = props.errorMsg
+        Items.value[0].validate = false
+        Items.value[0].Message = Items.value[0].errorMsg
 
     }
+})
+watch(EmailValue,()=>{
+     const regex = Items.value[1].regex
+       Items.value[1].val = EmailValue.value
+    if (EmailValue.value == "") {
+        Items.value[1].Message = 'this field reqired'
+    }
+    else if (regex.test(EmailValue.value)) {
+        Items.value[1].validate = true
+        Items.value[1].Message = Items.value[1].successMsg
+    }
+    else {
+        Items.value[1].validate = false
+        Items.value[1].Message = Items.value[1].errorMsg
+
+    }
+})
+watch(NumberValue,()=>{
+     const regex = Items.value[2].regex
+       Items.value[2].val = NumberValue.value
+    if (NumberValue.value == "") {
+        Items.value[2].Message = 'this field reqired'
+    }
+    else if (regex.test(NumberValue.value)) {
+        Items.value[2].validate = true
+        Items.value[2].Message = Items.value[2].successMsg
+    }
+    else {
+        Items.value[2].validate = false
+        Items.value[2].Message = Items.value[2].errorMsg
+
+    }
+})
+watch(organition,()=>{
+     const regex = Items.value[3].regex
+       Items.value[3].val = organition.value
+    if (organition.value == "") {
+        Items.value[3].Message = 'this field reqired'
+    }
+    else if (regex.test(organition.value)) {
+        Items.value[3].validate = true
+        Items.value[3].Message = Items.value[3].successMsg
+    }
+    else {
+        Items.value[3].validate = false
+        Items.value[3].Message = Items.value[3].errorMsg
+
+    }
+})
+watch(messages,()=>{
+     const regex = Items.value[4].regex
+       Items.value[4].val = messages.value
+    if (messages.value == "") {
+        Items.value[4].Message = 'this field reqired'
+    }
+    else if (regex.test(messages.value)) {
+        Items.value[4].validate = true
+        Items.value[4].Message = Items.value[4].successMsg
+    }
+    else {
+        Items.value[3].validate = false
+        Items.value[3].Message = Items.value[3].errorMsg
+
+    }
+})
+watchEffect(()=>{
+    if (Items.value[0].validate == true && Items.value[1].validate == true && Items.value[2].validate == true && Items.value[3].validate == true && Items.value[4].validate == true) {
+       disabled.value = true
+      }
+      else{
+        disabled.value = false
+      }
+})
+function Submited() {
+    console.log(FurstName.value);
+    console.log(EmailValue.value);
+    console.log(NumberValue.value);
+    console.log(organition.value);
+    console.log(messages.value);
+    FurstName.value = ""
+    EmailValue.value = ""
+    NumberValue.value = ""
+    organition.value = ""
+    messages.value = ""
+    
 }
+
+
+
 </script>
 <template>
-    <div class="input-container mt-5 mb-5">
-        <input ref="email" id="firstname" :value="props.modelValue" v-on:input="emitInput($event)" class="input"
-            :style="val != '' ? (validate ? 'border:2px solid green' : 'border:2px solid red') : Message = ''"
-            type="text" placeholder=" " />
-        <div class="cut"></div>
-        <label for="firstname" class="placeholder">{{labelName}}</label>
-        <p ref="labelEmail" :style="validate ? 'color:green' : 'color:red'" class="text-xs z-50 absolute">{{ Message }}</p>
+   <div>
+    <form action="" class="w-full" @submit.prevent="Submited">
+    <div  v-for="(item,i) in Items" :key="i" class="input-container mt-5 mb-5">
+        <input ref="email" id="firstname" v-model="item.wer" class="input"
+            :style="item.val != '' ? (item.validate ? 'border:2px solid green' : 'border:2px solid red') : item.Message = ''"
+            type="text" placeholder=" "/>
+        <label for="firstname" class="placeholder font-Mregular font-normal text-[15px]">{{item.label}}</label>
+        <p ref="labelEmail" :style="item.validate ? 'color:green' : 'color:red'" class="text-xs z-50 absolute">{{ item.Message }}</p>
     </div>
+    <div>
+    <button :disabled="!disabled" :class="{'opacity-50': !disabled}" type="submit" class="bg-[#002F5F] text-white rounded-lg py-[12px] px-[30px] mt-3">Send
+            message</button>
+    </div>
+      </form>
+   </div>
 </template>
 <style scoped>
 .input-container {
@@ -59,33 +204,14 @@ function emitInput(event: any) {
     width: 100%;
 }
 
-.cut {
-    background-color: white;
-    border-radius: 10px;
-    height: 20px;
-    left: 20px;
-    position: absolute;
-    top: -20px;
-    transform: translateY(0);
-    transition: transform 200ms;
-    width: 75px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-}
 
 .input:focus {
     border: 2px solid cyan;
 }
 
-.input:focus~.cut,
-.input:not(:placeholder-shown)~.cut {
-    transform: translateY(8px);
-}
-
 .placeholder {
-    color: black;
-    font-family: sans-serif;
+    background-color: white;
+    color: rgba(0, 0, 0, 0.38);
     left: 20px;
     line-height: 14px;
     pointer-events: none;
@@ -93,11 +219,13 @@ function emitInput(event: any) {
     transform-origin: 0 50%;
     transition: transform 200ms, color 200ms;
     top: 20px;
+    padding-left: 5px;
+    padding-right: 5px;
 }
 
 .input:focus~.placeholder,
 .input:not(:placeholder-shown)~.placeholder {
-    transform: translateY(-30px) translateX(10px) scale(0.75);
+    transform: translateY(-25px) translateX(-5px) scale(0.75);
 }
 
 .input:not(:placeholder-shown)~.placeholder {
--- components/getInTouch/formMain.vue
+++ components/getInTouch/formMain.vue
@@ -2,16 +2,6 @@
 import { ref,watch } from 'vue';
 import baseInput from './baseInput.vue';
 import BaseIcon from '~/components/BaseComponents/BaseIcons/BaseIcon.vue';
-let fullName = ref<string>('')
-let email = ref<string>('')
-let number = ref<string>('')
-
-function Submited() {
-    // isEmailValid()
-    console.log(email.value , fullName.value);
-
-}
-
 const items = ref<any[]>([
   {icon: 'Phone', name: '+998 88 188 90 00'},
   {icon: 'Email', name: 'ssolitionuz@gmail.com'},
@@ -36,104 +26,9 @@ const items = ref<any[]>([
             </div>
         </div>
         <div class="md:w-2/5 w-full"  >
-            <form action="" class="w-full" @submit.prevent="Submited">
-                <baseInput v-model="fullName" :regex="/([a-zA-Z]{3,30}\s*)+/"
-                    success-msg="All is good" error-msg="text must be Full name"
-                    label-name="Full name"></baseInput>
-                <baseInput v-model="email" :regex="/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/"
-                    success-msg="All is good" error-msg="text must be email"
-                    label-name="Email"></baseInput>
-                <baseInput v-model="number" :regex="/^[+]?998[012345789][0-9]{8}$/"
-                    success-msg="All is good" error-msg="text must be Uzb Phone"
-                    label-name="Phone Number"></baseInput>
-                <div class="input-container ic1">
-                    <input id="firstname" class="input" type="text" placeholder=" " />
-                    <div class="cut"></div>
-                    <label for="firstname" class="placeholder">Organisation</label>
-                </div>
-                <div class="input-container ic1">
-                    <input id="firstname" class="input" type="text" placeholder=" " />
-                    <div class="cut"></div>
-                    <label for="firstname" class="placeholder">Message</label>
-                </div>
-                <div>
-                    <button type="submit" class="bg-[#002F5F] text-white rounded-lg py-[12px] px-[30px] mt-3">Send
-                        message</button>
-                </div>
-            </form>
+                <baseInput></baseInput>   
         </div>
     </div>
 </template>
 <style scoped>
-.input-container {
-    height: 50px;
-    position: relative;
-    width: 100%;
-}
-
-.ic1 {
-    margin-top: 20px;
-}
-
-.input {
-    background-color: white;
-    border-radius: 8px;
-    border: 2px solid #ededed;
-    box-sizing: border-box;
-    color: black;
-    font-size: 18px;
-    height: 100%;
-    outline: 0;
-    padding: 4px 20px 0;
-    width: 100%;
-}
-
-.cut {
-    background-color: white;
-    border-radius: 10px;
-    height: 20px;
-    left: 20px;
-    position: absolute;
-    top: -20px;
-    transform: translateY(0);
-    transition: transform 200ms;
-    width: 75px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-}
-
-.input:focus {
-    border: 2px solid cyan;
-}
-
-.input:focus~.cut,
-.input:not(:placeholder-shown)~.cut {
-    transform: translateY(8px);
-}
-
-.placeholder {
-    color: black;
-    font-family: sans-serif;
-    left: 20px;
-    line-height: 14px;
-    pointer-events: none;
-    position: absolute;
-    transform-origin: 0 50%;
-    transition: transform 200ms, color 200ms;
-    top: 20px;
-}
-
-.input:focus~.placeholder,
-.input:not(:placeholder-shown)~.placeholder {
-    transform: translateY(-30px) translateX(10px) scale(0.75);
-}
-
-.input:not(:placeholder-shown)~.placeholder {
-    color: black;
-}
-
-.input:focus~.placeholder {
-    color: black
-}
 </style>
