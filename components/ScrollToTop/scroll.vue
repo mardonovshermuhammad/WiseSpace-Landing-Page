776d1bd26543e206ab7fd6dbc70465aea9886539
shakhzod-star <77914936+shakhzod-star@users.noreply.github.com>
2022-11-01 10:07:05 +0500

    scrooll

---
+++ components/ScrollToTop/scroll.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts" >
+import BaseIcon from '~/components/BaseComponents/BaseIcons/BaseIcon.vue'
+
+function scrollToTop() {
+    window.scrollTo({
+        top:0 ,
+        behavior : 'smooth'
+    })
+}
+</script>
+
+<template>
+    <button @click="scrollToTop" class="flex items-center bottom-0 absolute bottom justify-center  bg-[#002F5F] w-[50px] h-[50px]">
+        <BaseIcon name="ArrowTop" > 
+        </BaseIcon>
+    </button>
+</template>
